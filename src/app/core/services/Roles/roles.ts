663d61c3108e4408a989d3647f9329486331f4e0
AFFERY AUXENCE DELORS CHABEHOU <afferyauxencedelorschabehou@gmail.com>
2026-02-11 11:25:12 +0000

    Deploiement vercel V3

--- src/app/core/services/Roles/roles.ts
+++ src/app/core/services/Roles/roles.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError } from 'rxjs';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 import { ApiResponseData } from '../../models/Common/ApiResponseData';
 import { RoleDto } from '../../models/Core/Roles/DTOs/RoleDto';
 import { RoleRequest } from '../../models/Core/Roles/DTOs/RoleRequest';
