69099513ecf77e00889b20a2a9c9e33b4d6b5087
AFFERY AUXENCE DELORS CHABEHOU <afferyauxencedelorschabehou@gmail.com>
2026-05-14 20:23:59 +0000

    feat: Refonte logistik partie 6

--- src/app/core/components/not-found/not-found.ts
+++ src/app/core/components/not-found/not-found.ts
@@ -1,12 +1,23 @@
+import { CommonModule, DecimalPipe, DatePipe, SlicePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NotificationComponent } from '../notification-component/notification-component';
+import { SidebarComponent } from '../sidebar-component/sidebar-component';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+    imports: [
+    CommonModule,
+    RouterModule
+  ],
   standalone: true,
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
 })
 export class NotFound {
+
+  goBack(): void {
+  window.history.back();
+}
 
 }
